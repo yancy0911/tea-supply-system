694774bea929305d0f79d29e406c1fcb7e760efa
TINGTING FU <tingtingfu@TINGTINGdeMacBook-Air.local>
2026-03-22 10:42:00 -0400

    fix global admin changelist error

--- tea_supply/settings.py
+++ tea_supply/settings.py
@@ -60,7 +60,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tea_supply',
+    'tea_supply.apps.TeaSupplyConfig',
     'main',
 ]
 
